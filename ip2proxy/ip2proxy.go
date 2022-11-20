1979f50b9b5d93169c7156a16af65713afb8eb5f
pg9182 <96569817+pg9182@users.noreply.github.com>
2022-11-20 01:52:48 -0500

    ip2proxy: Add doc comments for record fields
    https://www.ip2location.com/database/px11-ip-proxytype-country-region-city-isp-domain-usagetype-asn-lastseen-threat-residential-provider

--- ip2proxy/ip2proxy.go
+++ ip2proxy/ip2proxy.go
@@ -161,19 +161,84 @@ type Record struct {
 	// the database.
 	Fields Field
 
+	// Two-character country code based on ISO 3166.
 	CountryShort string
-	CountryLong  string
-	Region       string
-	City         string
-	ISP          string
-	ProxyType    string
-	Domain       string
-	UsageType    string
-	ASN          string
-	AS           string
-	LastSeen     string
-	Threat       string
-	Provider     string
+
+	// Country name based on ISO 3166.
+	CountryLong string
+
+	// Region or state name.
+	Region string
+
+	// City name.
+	City string
+
+	// Internet Service Provider or company's name.
+	ISP string
+
+	// Type of proxy.
+	//   - (VPN) Anonymizing VPN services. These services offer users a publicly
+	//     accessible VPN for the purpose of hiding their IP address. Anonymity:
+	//     High.
+	//   - (TOR) Tor Exit Nodes. The Tor Project is an open network used by
+	//     those who wish to maintain anonymity. Anonymity: High.
+	//   - (DCH) Hosting Provider, Data Center or Content Delivery Network.
+	//     Since hosting providers and data centers can serve to provide
+	//     anonymity, the Anonymous IP database flags IP addresses associated
+	//     with them. Anonymity: Low.
+	//   - (PUB) Public Proxies. These are services which make connection
+	//     requests on a user's behalf. Proxy server software can be configured
+	//     by the administrator to listen on some specified port. These differ
+	//     from VPNs in that the proxies usually have limited functions compare
+	//     to VPNs. Anonymity: High.
+	//   - (WEB) Web Proxies. These are web services which make web requests
+	//     on a user's behalf. These differ from VPNs or Public Proxies in that
+	//     they are simple web-based proxies rather than operating at the IP
+	//     address and other ports level. Anonymity: High.
+	//   - (SES) Search Engine Robots. These are services which perform crawling
+	//     or scraping to a website, such as, the search engine spider or bots
+	//     engine. Anonymity: Low.
+	//   - (RES) Residential proxies. These services offer users proxy
+	//     connections through residential ISP with or without consents of
+	//     peers to share their idle resources. Only available with PX10 & PX11.
+	//     Anonymity: Medium.
+	ProxyType string
+
+	// Internet domain name associated with IP address range.
+	Domain string
+
+	// Usage type classification of ISP or company.
+	//   - (COM) Commercial
+	//   - (ORG) Organization
+	//   - (GOV) Government
+	//   - (MIL) Military
+	//   - (EDU) University/College/School
+	//   - (LIB) Library
+	//   - (CDN) Content Delivery Network
+	//   - (ISP) Fixed Line ISP
+	//   - (MOB) Mobile ISP
+	//   - (DCH) Data Center/Web Hosting/Transit
+	//   - (SES) Search Engine Spider
+	//   - (RSV) Reserved
+	UsageType string
+
+	// Autonomous system number (ASN).
+	ASN string
+
+	// Autonomous system (AS) name.
+	AS string
+
+	// Proxy last seen in days.
+	LastSeen string
+
+	// Security threat reported.
+	//   - (SPAM) Email and forum spammers
+	//   - (SCANNER) Network security scanners
+	//   - (BOTNET) Malware infected devices
+	Threat string
+
+	// Name of VPN provider if available.
+	Provider string
 }
 
 // IsValid checks whether the record exists in the database.
