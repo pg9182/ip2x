cf0f5cb140d431e76a19ecb2772657d123e34e73
pg9182 <96569817+pg9182@users.noreply.github.com>
2022-11-24 15:43:58 -0500

    test: Add fuzz tests

---
+++ test/fuzz_test.go
@@ -0,0 +1,35 @@
+package test
+
+import (
+	"net/netip"
+	"testing"
+)
+
+func FuzzLookup(f *testing.F) {
+	for _, ip := range ips {
+		hi, lo := addrUint128(ip)
+		f.Add(hi, lo)
+	}
+	f.Fuzz(func(t *testing.T, hi, lo uint64) {
+		var last string
+		as := []netip.Addr{
+			uint128Addr(hi, lo), // v6 (or v4 if hi is 0)
+			uint128Addr(0, lo&0xffffffff|0xffff00000000).Unmap(), // v4
+			uint128Addr(0, lo&0xffffffff|0xffff00000000),         //  ^ v4-mapped
+			uint128Addr(0x2002<<48|(lo&0xffffffff)<<16, 0),       //  ^ 6to4
+			uint128Addr(0x20010000<<32, ^(lo & 0xffffffff)),      //  ^ teredo
+		}
+		for i, a := range as {
+			r, err := IP2x_DB.Lookup(a)
+			if err != nil {
+				t.Errorf("lookup %s: %v", a, err)
+				// not fatal since r should still work on error
+			}
+			if res := r.FormatString(false, false); i >= 2 && last != res {
+				t.Errorf("lookup %s: expected all v4 mappings to match native v4 (%s): expected %q, got %q", a, as[1], last, res)
+			} else {
+				last = res
+			}
+		}
+	})
+}
